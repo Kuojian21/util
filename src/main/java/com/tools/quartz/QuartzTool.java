729279085cdfab50ebf0c2d05d6a37e3226118f6
bjzhangkuojian <bjzhangkuojian@corp.netease.com>
2017-09-01 14:19:14 +0800

    提交

---
+++ src/main/java/com/tools/quartz/QuartzTool.java
@@ -0,0 +1,33 @@
+package com.tools.quartz;
+
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.SchedulerFactory;
+import org.quartz.impl.StdSchedulerFactory;
+
+public class QuartzTool {
+
+	private static final SchedulerFactory factory = new StdSchedulerFactory();
+
+	public static Scheduler scheduler() {
+		try {
+			Scheduler scheduler = factory.getScheduler();
+			if (!scheduler.isStarted()) {
+				scheduler.start();
+			}
+			return scheduler;
+		} catch (SchedulerException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	public static void addJob(){
+		
+	}
+	
+	public static void deleteJob(){
+		
+	}
+
+}
