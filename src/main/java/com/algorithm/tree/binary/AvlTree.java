d9bf98ad834704153c24e3ac9d4f43c8ff19ae37
bjzhangkuojian <bjzhangkuojian@corp.netease.com>
2018-01-30 10:14:17 +0800

    tj3

--- src/main/java/com/algorithm/tree/binary/AvlTree.java
+++ src/main/java/com/algorithm/tree/binary/AvlTree.java
@@ -1,11 +1,13 @@
 package com.algorithm.tree.binary;
 
+import java.util.Random;
+
 public class AvlTree<T> {
 
 	public AvlNode<T> insert(AvlNode<T> node, T data) {
 		if (node == null) {
 			node = new AvlNode<T>(data);
-		} else if (this.compareTo(node.data, data) < 0) {
+		} else if (this.compareTo(node.data, data) > 0) {
 			node.left = this.insert(node.left, data);
 			if (this.height(node.left) - this.height(node.right) >= 2) {
 				if (this.height(node.left.left) > this.height(node.left.right)) {
@@ -16,7 +18,7 @@ public class AvlTree<T> {
 			} else {
 				node.height = Math.max(this.height(node.left), this.height(node.right)) + 1;
 			}
-		} else if (this.compareTo(node.data, data) > 0) {
+		} else if (this.compareTo(node.data, data) < 0) {
 			node.right = this.insert(node.right, data);
 			if (this.height(node.right) - this.height(node.left) >= 2) {
 				if (this.height(node.right.right) > this.height(node.right.left)) {
@@ -46,13 +48,15 @@ public class AvlTree<T> {
 		if (node == null) {
 			return -1;
 		}
-		return node.height;
+
+		return Math.max(this.height(node.left), this.height(node.right)) + 1;
+		// return node.height;
 	}
 
 	private AvlNode<T> ll(AvlNode<T> node) {
 		AvlNode<T> t = node;
 		node = node.left;
-		t.right = node.right;
+		t.left = node.right;
 		node.right = t;
 		t.height -= 1;
 		return node;
@@ -70,6 +74,7 @@ public class AvlTree<T> {
 	}
 
 	private AvlNode<T> lr(AvlNode<T> node) {
+		System.out.println("lr");
 		node.left = rr(node.left);
 		node.left.height += 1;
 		return ll(node);
@@ -77,47 +82,55 @@ public class AvlTree<T> {
 	}
 
 	private AvlNode<T> rl(AvlNode<T> node) {
+		System.out.println("rl");
 		node.right = ll(node.right);
 		node.right.height += 1;
 		return rr(node);
 
 	}
-	
-	
+
 	public void traverse(AvlNode<T> node) {
-		System.out.println(node.data);
-		if(node.left != null) {
+		if (node.left != null) {
+			System.out.print(node.left.data + "\t");
+		}
+
+		if (node.right != null) {
+			System.out.print(node.right.data + "\t");
+		}
+		
+		System.out.println();
+		
+		if (node.left != null) {
 			this.traverse(node.left);
 		}
-		
-		if(node.right != null) {
+
+		if (node.right != null) {
 			this.traverse(node.right);
 		}
-		
+
 	}
-	
-	
+
 	public static void main(String[] args) {
 		AvlTree<Integer> tree = new AvlTree<Integer>();
 		AvlNode<Integer> root = null;
-		for(int i = 0; i< 100;i++) {
-			root = tree.insert(root, i);
+		Random random = new Random();
+		for (int i = 0; i < 100; i++) {
+			root = tree.insert(root, random.nextInt(100));
 		}
-		System.out.println(root);
-		
-		tree.traverse(root);
-//		new Transfer<Integer>().transfer(root);
-		
+//		System.out.println(root);
+
+//		tree.traverse(root);
+		new Transfer<Integer>().transfer(root);
+
 		AvlNode<Integer> node = root;
-		while(node.left!=null) {
+		while (node.left != null) {
 			node = node.left;
-		};
-		
+		}
+
 		do {
-			System.out.println(node.data);
+			System.out.print(node.data + "\t\n");
 			node = node.right;
-		}while(node.right!=null);
-		
-		
+		} while (node != null);
+
 	}
 }
