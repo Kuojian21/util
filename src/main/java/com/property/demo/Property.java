813215fba536442d90e4b05a31c613eb3ad7267f
bjzhangkuojian <bjzhangkuojian@corp.netease.com>
2018-02-12 11:24:58 +0800

    提交

---
+++ src/main/java/com/property/demo/Property.java
@@ -0,0 +1,31 @@
+package com.property.demo;
+
+import java.lang.reflect.InvocationTargetException;
+
+import org.apache.commons.beanutils.PropertyUtils;
+
+import com.alibaba.fastjson.JSON;
+
+public class Property {
+	public static class A{
+		private String a = "a";
+		private String b = "b";
+		public String getA() {
+			return a;
+		}
+		public void setA(String a) {
+			this.a = a;
+		}
+		public String getB() {
+			return b;
+		}
+		public void setB(String b) {
+			this.b = b;
+		}
+	}
+	
+	public static void main(String[] args) throws IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+		System.out.println(JSON.toJSONString(PropertyUtils.describe(new A())));
+	}
+	
+}
--- src/main/java/com/tools/bean/PropertyUtils.java
+++
@@ -1,5 +0,0 @@
-package com.tools.bean;
-
-public class PropertyUtils {
-
-}
