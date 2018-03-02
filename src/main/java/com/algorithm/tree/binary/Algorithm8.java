07a731b58cfcc07b5c2a12605418f72bb142a260
bjzhangkuojian <bjzhangkuojian@corp.netease.com>
2018-03-02 14:27:35 +0800

    tij

--- src/main/java/com/algorithm/tree/binary/Algorithm8.java
+++ src/main/java/com/algorithm/tree/binary/Algorithm8.java
@@ -3,6 +3,7 @@ package com.algorithm.tree.binary;
 public class Algorithm8 {
 	/**
 	 * 3. ★用一种算法来颠倒一个链接表的顺序。现在在不用递归式的情况下做一遍。
+	 * 
 	 * @param root
 	 * @return
 	 */
@@ -15,11 +16,11 @@ public class Algorithm8 {
 		root.next = null;
 		return head;
 	}
-	
+
 	public LinkNode reverse2(LinkNode root) {
 		LinkNode head = null;
 		LinkNode node = null;
-		while(root != null) {
+		while (root != null) {
 			node = root.next;
 			root.next = head;
 			head = root;
@@ -27,4 +28,34 @@ public class Algorithm8 {
 		}
 		return head;
 	}
+	
+	/**
+	 * 用一种算法使通用字符串相匹配。
+	 * @param str
+	 * @param si
+	 * @param com
+	 * @param ci
+	 * @return
+	 */
+	public boolean match(char[] str, int si, char[] com, int ci) {
+		if (ci >= com.length && si >= str.length) {
+			return true;
+		}
+		if (ci >= com.length) {
+			return false;
+		}
+		if (com[ci] == '*') {
+			do {
+				if (match(str, si++, com, ci + 1)) {
+					return true;
+				}
+			} while (si < str.length);
+			return false;
+		} else if (com[ci] == '?' || com[ci] == str[si]) {
+			return match(str, si + 1, com, ci + 1);
+		} else {
+			return false;
+		}
+	}
+
 }
