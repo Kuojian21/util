316f5cbded188ea882390415e3d555777282eabc
Kuojian21 <Kuojian21@163.com>
2018-05-01 00:59:11 +0800

    tj

--- src/test/java/com/test/ftp/JschUtilTest.java
+++ src/test/java/com/test/ftp/JschUtilTest.java
@@ -13,19 +13,24 @@ public class JschUtilTest {
 	public void upload1() {
 		for (int i = 0; i < 10; i++) {
 			JschUtil.uploadSftp(JschUtil.sftp("trans.kaifae.com", 2022, "LMLC", "9X9XWa$f"),
-					StreamTool.toInputStream(new String("Hello World!").getBytes()),
-					"/upload/test", "test" + i + ".txt", true, true);
+					StreamTool.toInputStream(new String("Hello World!").getBytes()), "/upload/test",
+					"test" + i + ".txt", true, true);
 		}
 
 	}
 
 	@Test
 	public void upload2() throws UnsupportedEncodingException {
-		JschUtil.uploadSftp(JschUtil.sftp("123.57.157.2", 22, "lmlctest",
-				"SPLIT_classes:classes/exchange_njjjs.pub",
-				"SPLIT_classes:classes/exchange_njjjs.ppk", "njjjs".getBytes("UTF-8")),
-				StreamTool.toInputStream(new String("Hello World!123").getBytes()),
-				"upload", "test.txt", true, true);
+		try {
+			JschUtil.uploadSftp(
+					JschUtil.sftp("123.57.157.2", 22, "lmlctest", "SPLIT_classes:classes/exchange_njjjs.pub",
+							"SPLIT_classes:classes/exchange_njjjs.ppk", "njjjs".getBytes("UTF-8")),
+					StreamTool.toInputStream(new String("Hello World!123").getBytes()), "upload", "test.txt", true,
+					true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
 
 }
