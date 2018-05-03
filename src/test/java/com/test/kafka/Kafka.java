cfd50ca6f9ca307ef4e2213c5393d7bdb2f886fb
bjzhangkuojian <bjzhangkuojian@corp.netease.com>
2018-05-03 14:36:40 +0800

    提交

--- src/test/java/com/test/kafka/Kafka.java
+++ src/test/java/com/test/kafka/Kafka.java
@@ -16,19 +16,38 @@ public class Kafka {
 
 	public static void main(String[] args) {
 		String topic = "KJ_TEST";
+		Properties props = new Properties();
+		props.put("bootstrap.servers", "10.200.142.35:9090,10.200.142.36:9090,10.200.142.37:9090");
+		props.put("acks", "0");
+		props.put("retries", 0);
+		props.put("batch.size", 16384);
+		props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		Producer<String, String> producer = new Producer<String, String>(props);
 		new Thread(new Runnable() {
 			@Override
 			public void run() {
-				Properties props = new Properties();
-				props.put("bootstrap.servers", "10.200.142.35:9090,10.200.142.36:9090,10.200.142.37:9090");
-				props.put("acks", "0");
-				props.put("retries", 0);
-				props.put("batch.size", 16384);
-				props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-				props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-				Producer<String, String> producer = new Producer<String, String>(props);
 				while (true) {
 					producer.send(new ProducerRecord<String, String>(topic, "201803292024CORDER49316246"),
+							new Callback() {
+								@Override
+								public void onCompletion(RecordMetadata metadata, Exception exception) {
+									/* new Exception().printStackTrace(); */
+									if (exception != null) {
+										exception.printStackTrace();
+									}
+								}
+							});
+				}
+
+			}
+
+		}).start();
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				while (true) {
+					producer.send(new ProducerRecord<String, String>(topic, "201803292024CORDER49316247"),
 							new Callback() {
 								@Override
 								public void onCompletion(RecordMetadata metadata, Exception exception) {
