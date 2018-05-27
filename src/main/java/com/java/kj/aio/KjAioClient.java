0667af125a8a3473cbf9550c5dbe1936df954bf2
Kuojian21 <Kuojian21@163.com>
2018-05-27 09:16:03 +0800

    tj

--- src/main/java/com/java/kj/aio/KjAioClient.java
+++ src/main/java/com/java/kj/aio/KjAioClient.java
@@ -16,58 +16,57 @@ import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 
 public class KjAioClient {
 
-	private final GenericObjectPool<AsynchronousSocketChannel> pool;
+	private final GenericObjectPool<Holder> pool;
 
 	public KjAioClient(String ip, int port) {
 		GenericObjectPoolConfig config = new GenericObjectPoolConfig();
 		config.setMinIdle(-1);
 		config.setMaxTotal(100);
 		config.setMaxWaitMillis(30000);
-		pool = new GenericObjectPool<AsynchronousSocketChannel>(
-				new BasePooledObjectFactory<AsynchronousSocketChannel>() {
+		pool = new GenericObjectPool<Holder>(new BasePooledObjectFactory<Holder>() {
+			@Override
+			public PooledObject<Holder> wrap(Holder holder) {
+				return new DefaultPooledObject<Holder>(holder) {
 					@Override
-					public PooledObject<AsynchronousSocketChannel> wrap(AsynchronousSocketChannel channel) {
-						return new DefaultPooledObject<AsynchronousSocketChannel>(channel) {
-							@Override
-							public void invalidate() {
-								try {
-									channel.close();
-								} catch (IOException e) {
-									e.printStackTrace();
-								}
-								super.invalidate();
-							}
-						};
+					public void invalidate() {
+						try {
+							holder.channel.close();
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+						super.invalidate();
+					}
+				};
+			}
+
+			@Override
+			public Holder create() throws Exception {
+				AsynchronousSocketChannel channel = AsynchronousSocketChannel.open();
+				CountDownLatch latch = new CountDownLatch(1);
+				channel.connect(new InetSocketAddress(ip, port), null, new CompletionHandler<Void, Void>() {
+					@Override
+					public void completed(Void result, Void attachment) {
+						latch.countDown();
 					}
 
 					@Override
-					public AsynchronousSocketChannel create() throws Exception {
-						AsynchronousSocketChannel channel = AsynchronousSocketChannel.open();
-						CountDownLatch latch = new CountDownLatch(1);
-						channel.connect(new InetSocketAddress(ip, port), null, new CompletionHandler<Void, Void>() {
-							@Override
-							public void completed(Void result, Void attachment) {
-								latch.countDown();
-							}
-
-							@Override
-							public void failed(Throwable exc, Void attachment) {
-								latch.countDown();
-							}
-						});
-						latch.await();
-						return channel;
-
+					public void failed(Throwable exc, Void attachment) {
+						latch.countDown();
 					}
-				}, config);
+				});
+				latch.await();
+				Holder holder = new Holder(channel, 1024);
+				return holder;
+
+			}
+		}, config);
 		Runtime.getRuntime().addShutdownHook(new Thread() {
 			public void run() {
 				KjAioClient.this.pool.close();
 			}
 		});
 	}
-	
-	
+
 	public static class Holder {
 		public final AsynchronousSocketChannel channel;
 		public final ByteBuffer reader;
@@ -79,7 +78,26 @@ public class KjAioClient {
 			this.semaphore = new Semaphore(1);
 		}
 	}
-	
+
+	public CompletionHandler<Integer, Holder> READER = new CompletionHandler<Integer, Holder>() {
+
+		@Override
+		public void completed(Integer result, Holder attachment) {
+			ByteBuffer read = attachment.reader;
+			read.flip();
+			byte[] data = new byte[read.limit()];
+			read.get(data);
+			read.rewind();
+			attachment.channel.read(read, attachment, READER);
+		}
+
+		@Override
+		public void failed(Throwable exc, Holder attachment) {
+
+		}
+
+	};
+
 	public CompletionHandler<Integer, Holder> WRITER = new CompletionHandler<Integer, Holder>() {
 
 		@Override
@@ -95,20 +113,33 @@ public class KjAioClient {
 	};
 
 	public static interface Action {
-		void request(AsynchronousSocketChannel channel);
+		void request(Holder holder) throws Exception;
 	}
 
 	public void request(Action action) {
-		AsynchronousSocketChannel channel = null;
+		Holder holder = null;
 		try {
-			channel = pool.borrowObject();
-			action.request(channel);
+			holder = pool.borrowObject();
+			action.request(holder);
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
-			if (channel != null) {
-				pool.returnObject(channel);
+			if (holder != null && holder.channel.isOpen()) {
+				pool.returnObject(holder);
 			}
 		}
+	}
+
+	public byte[] request(byte[] request) {
+
+		this.request(new Action() {
+			@Override
+			public void request(Holder holder) throws Exception {
+				holder.semaphore.acquire();
+				holder.channel.write(ByteBuffer.wrap(request), holder, WRITER);
+			}
+
+		});
+		return null;
 	}
 }
--- src/test/java/com/test/aio/AioClientTest.java
+++ src/test/java/com/test/aio/AioClientTest.java
@@ -1,10 +1,10 @@
 package com.test.aio;
 
 import java.nio.ByteBuffer;
-import java.nio.channels.AsynchronousSocketChannel;
 import java.util.concurrent.TimeUnit;
 
 import com.java.kj.aio.KjAioClient;
+import com.java.kj.aio.KjAioClient.Holder;
 
 public class AioClientTest {
 
@@ -12,21 +12,16 @@ public class AioClientTest {
 		KjAioClient client = new KjAioClient("127.0.0.1", 8888);
 		client.request(new KjAioClient.Action() {
 			@Override
-			public void request(AsynchronousSocketChannel channel) {
-				KjAioClient.Holder holder = new KjAioClient.Holder(channel,1024);
+			public void request(Holder holder) throws InterruptedException {
 				for (int i = 0; i < 100000000; i++) {
-					byte[] data  = new byte[4];  
-			        data[0] = (byte)((i >> 24) & 0xff); 
-			        data[1] = (byte)((i >> 16)& 0xff );  
-			        data[2] = (byte)((i >> 8) & 0xff );  
-			        data[3] = (byte)((i >> 0) & 0xff ); 
-			        System.out.println(i);
-			        try {
-						holder.semaphore.acquire();
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-					channel.write(ByteBuffer.wrap(data),10,TimeUnit.MINUTES,holder,client.WRITER);
+					byte[] data = new byte[4];
+					data[0] = (byte) ((i >> 24) & 0xff);
+					data[1] = (byte) ((i >> 16) & 0xff);
+					data[2] = (byte) ((i >> 8) & 0xff);
+					data[3] = (byte) ((i >> 0) & 0xff);
+					System.out.println(i);
+					holder.semaphore.acquire();
+					holder.channel.write(ByteBuffer.wrap(data), 10, TimeUnit.MINUTES, holder, client.WRITER);
 				}
 			}
 		});
