80d225cfef0a965b462f2d154230cc4fa7cd8895
bjzhangkuojian <bjzhangkuojian@corp.netease.com>
2018-06-01 16:40:16 +0800

    提交

--- src/main/java/com/algorithm/tree/binary/Algorithm18.java
+++ src/main/java/com/algorithm/tree/binary/Algorithm18.java
@@ -3,7 +3,7 @@ package com.algorithm.tree.binary;
 /**
  * 第18 题：
 题目：n 个数字（0,1,…,n-1）形成一个圆圈，从数字0 开始，
-每次从这个圆圈中删除第m 个数字（第一个为当前数字本身，第二个为当前数字的下一个数
+（第一个为当前数字本身，第二个为当前数字的下一个数
 字）。
 当一个数字删除后，从被删除数字的下一个继续删除第m 个数字。
 求出在这个圆圈中剩下的最后一个数字。
@@ -43,6 +43,6 @@ public class Algorithm18 {
 	
 	
 	public static void main(String[] args) {
-		System.out.println(new Algorithm18().func2(3,2));
+		System.out.println(new Algorithm18().func2(5,2));
 	}
 }
--- src/main/java/com/algorithm/tree/binary/Algorithm19.java
+++ src/main/java/com/algorithm/tree/binary/Algorithm19.java
@@ -33,8 +33,8 @@ public class Algorithm19 {
 	}
 	
 	public static void main(String[] args) {
-		System.out.println(new Algorithm19().fibonacci(10));
-		System.out.println(new Algorithm19().fibonacci1(10));
+		System.out.println(new Algorithm19().fibonacci(100));
+		System.out.println(new Algorithm19().fibonacci1(10000000));
 	}
 	
 }
