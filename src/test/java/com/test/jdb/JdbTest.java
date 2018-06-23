6570db6b1ef29e63b749e8a5927fa0209300b8fd
Kuojian21 <Kuojian21@163.com>
2018-06-23 22:43:53 +0800

    tj

---
+++ src/test/java/com/test/jdb/JdbTest.java
@@ -0,0 +1,12 @@
+package com.test.jdb;
+
+public class JdbTest {
+
+	public static void main(String[] args) throws InterruptedException {
+		while(true) {
+			System.out.println("日志");
+			Thread.sleep(3000);
+		}
+	}
+
+}
