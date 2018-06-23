ac6d5d31111f1b27f6db4937bf8192a4edc8fd75
Kuojian21 <Kuojian21@163.com>
2018-06-23 23:19:53 +0800

    tj

--- src/test/java/com/test/jdb/JdbTest.java
+++ src/test/java/com/test/jdb/JdbTest.java
@@ -1,12 +1,35 @@
 package com.test.jdb;
 
+/**
+ * 
+ * @author kuojian21
+ *
+ *  -agentlib:jdwp=transport=dt_socket,address=8000,server=y,suspend=n
+ *  -Xdebug -Xrunjdwp,transport=dt_socket,server=y,address=5432,suspend=n,onthrow=java.io.IOException,launch=/sbin/echo
+ */
 public class JdbTest {
 
+	/**
+	 * 
+	 * @param args
+	 * @throws InterruptedException
+	 */
 	public static void main(String[] args) throws InterruptedException {
-		while(true) {
-			System.out.println("日志");
-			Thread.sleep(3000);
+		while (true) {
+			int i = 5;
+			int j = 6;
+			int sum = add(i, j);
+			System.out.println(sum);
+			sum = 0;
+			for (i = 0; i < 100; i++)
+				sum += i;
+			System.out.println(sum);
+			Thread.sleep(2000);
 		}
 	}
-
+	public static int add(int augend, int addend) {
+		int sum = augend + addend;
+		return sum;
+	}
+	
 }
